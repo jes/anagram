15d2580341047457ed449f48bddcc287dad94e9a
James Stanley <james@incoherency.co.uk>
2013-08-03 14:28:19 +0100

    Comments

--- anagram.js
+++ anagram.js
@@ -12,6 +12,7 @@
 
         var count = {};
 
+        /* get the net change in usage of each letter (input letters minus used letters) */
         for (var i = 0; i < letters.length; i++) {
             if (!count.hasOwnProperty(letters[i]))
                 count[letters[i]] = 0;
@@ -26,6 +27,7 @@
         var output = [];
         var excess = [];
 
+        /* get a list of available and excessively-used letters */
         for (var letter in count) {
             if (!letter.match(/[A-Z]/))
                 continue;
@@ -35,28 +37,33 @@
                 excess.push(letter);
         }
 
+        /* shuffle to make it easy to spot patterns */
         output = shuffle(output).join('');
         excess = shuffle(excess).join('');
 
         $('#unused-letters').html(output);
         $('#excess-letters').html(excess);
 
+        /* try to complete the word the user is typing */
         var inputs = $('#anagram-letters').val().split(' ');
         if (inputs.length > 0) {
             var lastword = inputs[inputs.length-1].toLowerCase();
 
             var words = [];
-            var dictnode = dictionary;
 
+            /* find the node in the trie corresponding to this prefix */
+            var dictnode = dictionary;
             for (var i = 0; dictnode && i < lastword.length; i++)
                 dictnode = dictnode[lastword[i]];
 
+            /* if there is a dictionary node here, recurse through it remembering the words */
             if (dictnode) {
                 _recurse_solve_letters(output.toLowerCase(), dictnode, {}, function(word) {
                     words.push(word)
                 }, lastword);
             }
 
+            /* put longer words first */
             words.sort(function(a, b) { return b.length - a.length });
             $('#suggestions').html(words.join('\n'));
         }
